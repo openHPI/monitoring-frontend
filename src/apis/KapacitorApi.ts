713684d006690a9cd685201d0b17b3783e173d75
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2019-02-20 17:01:59 +0100

    Add additional KapacitorApi functions

--- src/apis/KapacitorApi.ts
+++ src/apis/KapacitorApi.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class KapacitorApi {
     // region public members
     // endregion
@@ -14,6 +16,28 @@ export default class KapacitorApi {
         const topics = await response.json();
         const test = topics.topics.find((topic: any) => topic.id === topicName);
         return test ? test.level : 'OK';
+    }
+
+    public static async taskVariables(taskName: string): Promise<any> {
+        const response = await fetch(`http://82.140.0.78:9092/kapacitor/v1/tasks/${taskName}`);
+        const topic = await response.json();
+        return topic.vars;
+    }
+
+    public static async updateTaskVariables(taskName: string, taskVariables: any): Promise<string> {
+        await axios.patch(`http://82.140.0.78:9092/kapacitor/v1/tasks/${taskName}`, {
+            vars: taskVariables,
+        });
+
+        await axios.patch(`http://82.140.0.78:9092/kapacitor/v1/tasks/${taskName}`, {
+            status: 'disabled',
+        });
+
+        await axios.patch(`http://82.140.0.78:9092/kapacitor/v1/tasks/${taskName}`, {
+            status: 'enabled',
+        });
+
+        return 'OK';
     }
     // endregion
 
