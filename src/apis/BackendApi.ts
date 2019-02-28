11070b4576ee1efc59f8a8c2e86a23245c692f45
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2019-02-28 16:15:05 +0100

    Add methods to snooze and unsnooze events

--- src/apis/BackendApi.ts
+++ src/apis/BackendApi.ts
@@ -16,6 +16,28 @@ export default class BackendApi {
         const topic = await response.json();
         return topic.events;
     }
+
+    public static async snoozeEvent(eventId: string, days: number): Promise<void> {
+        await fetch(`http://82.140.0.78:8082/events/snooze/`, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ eventId, days }),
+        });
+    }
+
+    public static async unsnoozeEvent(eventId: string): Promise<void> {
+        await fetch(`http://82.140.0.78:8082/events/unsnooze/`, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ eventId }),
+        });
+    }
     // endregion
 
     // region private methods
